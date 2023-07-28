6439e8945ccf74507b30d22eff6a951ac134e643
Fan.WU <fan.wu@navya.tech>
2023-07-28 20:45:42 +0200

    training works

--- model/auto_encoder.py
+++ model/auto_encoder.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 from torch.autograd import Variable
 from torchvision.utils import save_image
 
+import cv2
+
 
 import os
 
@@ -20,31 +22,41 @@ transform = transforms.Compose([
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class DataLoader():
-    def __init__(self, data_dir="../dataset/"):
+    def __init__(self, data_dir="./dataset/"):
         self.data_dir = data_dir
         self.data_list = []
         self.label_list = []
+        self.test_list = []
 
     def _get_files_names(self):
-        field_dir = self.data_dir + "fileds"
+        field_dir = os.path.join(self.data_dir,  "fields")
         road_dir = self.data_dir + "roads"
         for filename in os.listdir(field_dir):
             self.data_list.append(os.path.join(field_dir, filename))
-            self.label_list.append(0)
+            self.label_list.append(torch.tensor([0,1], dtype=float))
         for filename in os.listdir(road_dir):
             self.label_list.append(os.path.join(road_dir, filename))
-            self.label_list.append(1)
+            self.label_list.append(torch.tensor([1,0], dtype=float))
+
+    def get_test_file(self):
+        test_dir = self.data_dir + "test_images"
+        for filename in os.listdir(test_dir):
+            self.test_list.append(os.path.join(test_dir, filename))
 
     def load_img(self, index):
-        return self.data_list[index], self.label_list[index]
+        img = cv2.imread(self.data_list[index])
+        img = cv2.resize(img, (60,60), interpolation= cv2.INTER_LINEAR)
+        trans = transforms.ToTensor()
+        img = trans(img)
+        return img, self.label_list[index]
 
+    def load_test_img(self, name):
+        img =  cv2.imread(name)
+        img = img.transpose((2, 0, 1))
+        img = cv2.resize(img, (60,60))
+        return img
 
-def getTrainData(data_path="../data/"):
-     
     
-
-
-
 """ AutoencoderFR means Autoencoder Field Road"""
 class AutoencoderFR(nn.Module):
      def __init__(self):
@@ -61,51 +73,59 @@ class AutoencoderFR(nn.Module):
              nn.Conv2d(8, 16, kernel_size=3, stride=2, padding=1 ), #16x8x8
              nn.Dropout2d(p=0.1),
              nn.ReLU(True),
-             nn.Flatten(),
+             nn.Flatten(0,-1), 
              nn.Linear(1024, 2)
              )
-         self.softmax = nn.Softmax(dim=1)
+         #self.softmax = torch.nn.functional.softmax()
+         self.linear_decoder = nn.Linear(2,1024)
          self.decoder = nn.Sequential(
-             nn.Linear(2, 1024),
+             #nn.Linear(2, 1024),
              nn.ReLU(True),
              nn.Unflatten(1, (16, 8, 8)),
              nn.Dropout2d(p=0.1),
-             nn.ConvTranspose2d(16, 8, kernel_size=5, stride=2, padding=1),
              nn.ReLU(True),
-             nn.Upsample(scale_factor=2, mode='bilinear'),
+             nn.ConvTranspose2d(16, 8, kernel_size=3, stride=2, padding=1),
              nn.Dropout2d(p=0.1),
-             nn.ConvTranspose2d(8, 4, kernel_size=3, stride=1, padding=1), 
              nn.ReLU(True),
+             nn.ConvTranspose2d(8, 4, kernel_size=3, stride=1, padding=1), 
              nn.Upsample(scale_factor=2),
              nn.Dropout2d(p=0.1),
-             nn.Conv2d(4,3, kernel_size=3, stride=1, padding=1)
+             nn.ReLU(True),
+             nn.ConvTranspose2d(4,3, kernel_size=3, stride=1, padding=1),
+             nn.Upsample(scale_factor=2),
              )
          
      def forward(self, x):
          out_en = self.encoder(x)
-         out = self.softmax(out_en)
+         out_cl = torch.softmax(out_en, dim=0)
+         out = self.linear_decoder(out_en)
+         out = torch.unsqueeze(out,0)
          out = self.decoder(out)
-         return out, out_en
+         return out, out_cl
      
-model = AutoencoderFR().to(device)
+model = AutoencoderFR()#.to(device)
 distance   = nn.MSELoss()
 class_loss = nn.CrossEntropyLoss()
- 
+
 optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
  
 mse_multp = 0.5
 cls_multp = 0.5
  
 model.train()
- 
+
+dataloader = DataLoader()
+dataloader._get_files_names()
+
 for epoch in range(num_epochs):
     total_mseloss = 0.0
     total_clsloss = 0.0
-    for ind, data in enumerate(dataloader):
-        img, labels = data[0].to(device), data[1].to(device) 
-        output, output_en = model(img)
+    for index in range(len(dataloader.data_list)):
+        img, labels = dataloader.load_img(index) 
+        output, output_cl = model(img)
+        output = torch.squeeze(output)
         loss_mse = distance(output, img)
-        loss_cls = class_loss(output_en, labels)
+        loss_cls = class_loss(output_cl, labels)
         loss = (mse_multp * loss_mse) + (cls_multp * loss_cls)  # Combine two losses together
         optimizer.zero_grad()
         loss.backward()
@@ -115,10 +135,18 @@ for epoch in range(num_epochs):
         total_clsloss += loss_cls.item()
  
      # Check accuracy on test set after each epoch:
-    model.eval()   # Turn off dropout in evaluation mode
+    
+    print('epoch [{}/{}], loss_mse: {:.4f}  loss_cls: {:.4f}'.format(epoch+1, num_epochs, total_mseloss / len(dataloader.data_list), total_clsloss / len(dataloader.data_list)))
+
+
+
+
+"""
+
+model.eval()   # Turn off dropout in evaluation mode
     acc = 0.0
     total_samples = 0
-    for data in testloader:
+    for name in dataloader.test_list:
         # We only care about the 10 dimensional encoder output for classification
         img, labels = data[0].to(device), data[1].to(device) 
         _, output_en = model(img)   
@@ -128,5 +156,6 @@ for epoch in range(num_epochs):
         acc += (pred == labels.cpu().numpy()).sum()
         total_samples += labels.shape[0]
     model.train()   # Enables dropout back again
-    print('epoch [{}/{}], loss_mse: {:.4f}  loss_cls: {:.4f}  Acc on test: {:.4f}'.format(epoch+1, num_epochs, total_mseloss / len(dataloader), total_clsloss / len(dataloader), acc / total_samples))
 
+
+"""
